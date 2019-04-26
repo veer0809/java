444f9b59cb75879a110e9600c4c090a8d4ff47f7
hp <hp@Veer-PC>
2019-04-26 23:32:26 +0530

    main

---
+++ Inheritance/src/inherit/VehicleMain.java
@@ -0,0 +1,14 @@
+package inherit;
+
+public class VehicleMain {
+
+	public static void main(String[] args) {
+		
+		
+		Car c = new Car("ciaz", "White", 180 , "manual");
+		
+		c.printResult();
+
+	}
+
+}
